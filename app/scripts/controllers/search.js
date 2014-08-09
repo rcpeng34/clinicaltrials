61af4c90043266329e8095eb7ba9a02e32bb57a1
rcpeng34 <rcpeng@gmail.com>
2014-08-09 00:36:45 -0700

    Search now reaches gov site via node server and returns to console client side

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -1,16 +1,32 @@
 'use strict';
 
-var searchController = function($scope){
+var searchController = function($scope, $http){
+  var searchTerms = [];
   $scope.searching = false;
+
   $scope.search = function(){
     if ($scope.searchTerm){
-      alert('searching for ' + $scope.searchTerm);
       $scope.searching = true;
+      searchTerms = $scope.searchTerm.split(' ');
+      var searchURL = '/searchstudies?query=';
+      for (var i=0; i<searchTerms.length; i++){
+        searchURL += searchTerms[i].toString() + '+';
+      }
+      // searchURL ends in a '+' so remove it
+      searchURL = searchURL.substring(0, searchURL.length-1);
+      console.log('searching for ', searchURL);
+      $http({
+        method: 'GET',
+        url: searchURL
+      })
+      .success(function(data){
+        console.log('from http', data);
+      });
     } else {
-      alert('Search bar is empty!');
+      console.log('Search bar is empty!');
     }
   };
 };
 
 angular.module('clinicaltrialsApp')
-  .controller('SearchCtrl', ['$scope', searchController]);
+  .controller('SearchCtrl', ['$scope', '$http', searchController]);
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 "use strict";
 
 var express = require ('express'),
-    http    = require ('http'),
-    https   = require ('https');
+    http    = require ('http');
 
 var app = express();
 var httpport = process.env.port || 9000;
@@ -15,6 +14,41 @@ var server = app.listen(httpport, function() {
 app.use('/', express.static(__dirname + '/app'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/app/index.html');
-});
+app.get('/searchstudies', function(req, res){
+  var searchTerms = req.query.query.split(' ');
+
+  // generate request url path
+  // example http://clinicaltrial.gov/ct2/results?term=Stroke+AND+Brain&Search=Searc&displayxml=true
+  var searchPath = '/';
+
+  for(var i=0; i<searchTerms.length-1; i++){
+    searchPath += searchTerms[i] + '+AND+';
+  }
+  // add the last term
+  searchPath += searchTerms[searchTerms.length-1];
+  searchPath += '&Search=Searc&displayxml=true';
+
+  console.log('calling', searchPath);
+
+  var call = http.get({
+    host:'www.clinicaltrial.gov',
+    path: '/ct2/results?term=' + searchPath
+  }, function(response){
+    var repString = [];
+    console.log('success');
+    response.setEncoding('utf8');
+    response.on('data', function(chunk){
+      repString.push(chunk);
+    });
+    response.on('end', function(){
+      console.log('done clinicaltrial.gov request');
+    res.status(200).send(repString);
+    });
+  });
+
+  call.on('error', function(e){
+    console.log('error', e.message);
+  })
+  // res.status(200).send(searchTerms);
+
+})
