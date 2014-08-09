e35d41a6c3f5df76239c8aa4651f7ead8ac62034
rcpeng34 <rcpeng@gmail.com>
2014-08-08 19:30:56 -0700

    Auto redirects to /search

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,11 +9,11 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+      .when('/search', {
+        templateUrl: 'views/search.html',
+        controller: 'SearchCtrl'
       })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/explore'
       });
   });
