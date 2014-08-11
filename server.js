5aac7b1d8a47aaa96670bb9334f4c1637f5f97b7
rcpeng34 <rcpeng@gmail.com>
2014-08-10 22:34:52 -0700

    Makes requests to retrieve up to 100 trials and sends response as one array of trials

--- server.js
+++ server.js
@@ -17,44 +17,61 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/searchstudies', function(req, res){
   var searchTerms = req.query.query.split(' ');
+  var trials = [];
+  var searchPath= '';
+  var called = 0;
 
-  // generate request url path
-  // example http://clinicaltrial.gov/ct2/results?term=Stroke+AND+Brain&Search=Searc&displayxml=true
-  var searchPath = '/';
+  for (var j=1; j < 6; j++){
+    // make 5 calls to get 100 trials
+    // generate request url path
+    // example http://clinicaltrial.gov/ct2/results?term=Stroke+AND+Brain&Search=Searc&displayxml=true
+    searchPath = '/';
+    for(var i=0; i<searchTerms.length-1; i++){
+      searchPath += searchTerms[i] + '+AND+';
+    }
+    // add the last term
+    searchPath += searchTerms[searchTerms.length-1];
+    searchPath += '&Search=Searc&displayxml=true&pg=' + j;
 
-  for(var i=0; i<searchTerms.length-1; i++){
-    searchPath += searchTerms[i] + '+AND+';
-  }
-  // add the last term
-  searchPath += searchTerms[searchTerms.length-1];
-  searchPath += '&Search=Searc&displayxml=true';
-
-  console.log('calling', searchPath);
-
-  var call = http.get({
-    host:'www.clinicaltrial.gov',
-    path: '/ct2/results?term=' + searchPath
-  }, function(response){
-    var repString = '';
-    console.log('success');
-    response.setEncoding('utf8');
-    response.on('data', function(chunk){
-      repString+=chunk;
-    });
-    response.on('end', function(){
-      console.log('done clinicaltrial.gov request');
-      console.log(repString);
-      xml2js(repString, function(err, result){
-        if (err){
-          console.log('error parsing xml ', err);
-        } else {
-          res.status(200).send(JSON.stringify(result));
-        }
+    var call = http.get({
+      host:'www.clinicaltrial.gov',
+      path: '/ct2/results?term=' + searchPath
+    }, function(response){
+      var repString = '';
+      response.setEncoding('utf8');
+      response.on('data', function(chunk){
+        repString+=chunk;
+      });
+      response.on('end', function(){
+        // console.log('done clinicaltrial.gov request');
+        // console.log(repString);
+        xml2js(repString, function(err, result){
+          if (err){
+            console.log('error parsing xml ', err);
+          } else {
+            // extract clinical studies and add to trials
+            console.log(Object.keys(result));
+            called++;
+            // some searches might have less than 100 results, check if result has any studies
+            if (result.search_results.clinical_study){
+              console.log('inside secondary loop');
+              for (var k=0; k < result.search_results.clinical_study.length; k++){
+                console.log('pushing');
+                trials.push(result.search_results.clinical_study[k]);
+              }
+            }
+            // check if trials has 5 objects, if it does all calls completed
+            if (called === 5){
+              console.log(trials);
+              res.status(200).send(JSON.stringify(trials));
+            }
+          }
+        });
       });
     });
-  });
 
-  call.on('error', function(e){
-    console.log('error', e.message);
-  });
+    call.on('error', function(e){
+      console.log('error', e.message);
+    });
+  }
 });
